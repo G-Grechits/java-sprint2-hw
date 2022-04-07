58025d78e81333b1d7718f5ee06217c8a2aa7b0f
G.Grechits <g.grechits@yandex.ru>
2022-04-08 02:47:09 +0500

    fix: изменить модификатор доступа списка history на private и добавить константу ограничения MAX_HISTORY_SIZE

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    ArrayList<Task> history = new ArrayList<>();
+    public static final int MAX_HISTORY_SIZE = 10; //добавлена константа ограничения по количеству хранимых задач
+    private List<Task> history = new ArrayList<>(); //LinkedList мы ещё не проходили, поэтому пока не использую
 
     @Override
     public void add(Task task) {
         history.add(task);
-        if (history.size() > 10) { //добавлено ограничение по хранению только 10 задач
+        if (history.size() > MAX_HISTORY_SIZE) {
             history.remove(0);
         }
     }
